bfb37f3d693aa650e7e2235818cc7b4859203a23
coder1932010 <88028064+coder1932010@users.noreply.github.com>
2022-09-17 14:17:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ function check(){
 
 function speak(){
     var synth =window.speechSynthesis;
-    speak_data_1 = "The first predication is" + prediction_1;
-    speak_data_2 = "And the second prediction is" +prediction_2;
+    speak_data_1 = "The first prediction is  " + prediction_1;
+    speak_data_2 = "And the second prediction is  " +prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1+ speak_data_2);
     synth.speak(utterThis);
 }
